d424807398402c5567147fb2029fd5002caecd95
Vladimir Jelincic <vladimir.jelincic@gmail.com>
2016-02-28 10:24:36 +0100

    Initial commit

---
+++ textcircle.js
@@ -0,0 +1,39 @@
+this.Documents=new Mongo.Collection("documents");
+
+
+
+if (Meteor.isClient) {
+
+    Template.editor.helpers({
+        docid: function(){
+            console.log("doc id helper");
+            console.log(Documents.findOne());
+            var doc=Documents.findOne();
+            if(doc){
+                return doc._id
+            }
+            else {
+                return undefined;
+            }
+        },
+        config: function(){
+            return function(editor){
+                editor.on("change",function(cm_editor,info){
+                    console.log(cm_editor.getValue());
+                    $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+                })
+
+            }
+        }
+    }
+)
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+      if(!Documents.findOne()){
+          Documents.insert({title:"my new document"});
+      }
+  });
+}
